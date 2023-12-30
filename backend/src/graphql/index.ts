7f7966c2de975692afe63e5b0c2f6bbbdba61122
Alberto Valenzuela <avalen28@gmail.com>
2023-12-30 15:19:18 +0100

    Adds Faker for users

--- backend/src/graphql/index.ts
+++ backend/src/graphql/index.ts
@@ -1,3 +1,6 @@
+// ESM
+import { faker } from "@faker-js/faker";
+
 export const typeDefs = `
     type User {
         id: Int
@@ -8,16 +11,36 @@ export const typeDefs = `
         website: String
     }
 
+    type Post {
+        id: Int
+        title: String
+        content: String
+    }
+
     type Query {
         user(id: Int!): User
         users: [User]
     }
 `;
 
+function createRandomUser() {
+  return {
+    id: faker.number.int(),
+    name: faker.person.firstName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    phone: faker.phone.number(),
+    website: faker.internet.url(),
+  };
+}
+
+const USERS = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
+
 export const resolvers = {
   Query: {
     user() {},
     users() {},
   },
-  //   Mutation: {},
 };
