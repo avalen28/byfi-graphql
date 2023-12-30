809eea8474af96149e9356040926198f957c1916
Alberto Valenzuela <avalen28@gmail.com>
2023-12-30 12:19:38 +0100

    Adds initial server config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,3 +7,19 @@ import { expressMiddleware } from "@apollo/server/express4";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
+
+const initServer = async () => {
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/", (req, res) => {
+    res.send("Hello world");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+};
+
+initServer();
