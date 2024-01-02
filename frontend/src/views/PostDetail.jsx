fe6616e5e69cf54596adf51e6d5fc8e17dc7578d
Alberto Valenzuela <avalen28@gmail.com>
2024-01-02 20:28:11 +0100

    Adds details to post and user views

--- frontend/src/views/PostDetail.jsx
+++ frontend/src/views/PostDetail.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const PostDetail = () => {
+  const [post, setPost] = useState(null);
+  const getPostDetails = async () => {
+    try {
+      const postFromApi = await axios.get("http://localhost:8080/post/details");
+      setPost(postFromApi.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPostDetails();
+  }, []);
   return (
     <div>
       <Link to="/">Go back</Link>
+      {post && (
+        <div>
+          <h1>{post.title}</h1>
+          <h4>{post.body}</h4>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/views/UserDetail.jsx
+++ frontend/src/views/UserDetail.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const UserDetail = () => {
+  const [user, setUser] = useState(null);
+  const getUserDetails = async () => {
+    try {
+      const userFromApi = await axios.get("http://localhost:8080/user/details");
+      setUser(userFromApi.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  }, []);
+
   return (
     <div>
       <Link to="/">Go back</Link>
+      {user && (
+        <div>
+          <h1>{user.username}</h1>
+          <h4>Email: {user.email}</h4>
+          <h4>
+            Address: {user.address.street}, {user.address.city}
+          </h4>
+          <h4>Phone: {user.phone}</h4>
+        </div>
+      )}
     </div>
   );
 };
